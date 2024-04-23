838715290ef5eea358dfaac75589422ac469f275
vperked <perked@riseup.net>
2024-04-22 23:30:14 -0400

    Finishing touches.

--- src/main.go
+++ src/main.go
@@ -1,29 +1,38 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"os"
 )
 
 func main() {
+	fmt.Printf("Enter a valid file path : ")
 	openFile()
+
 }
 
 func openFile() {
-	fileLocation := "/Users/Perke/OneDrive/Desktop/FileReader/data/retard.json"
-	dat, err := os.ReadFile(fileLocation)
+	scanner := bufio.NewScanner(os.Stdin)
+	scanner.Scan()
+	err := scanner.Err()
+	fmt.Println("Reading:", scanner.Text())
+	if err != nil {
+		fmt.Println(err)
+	}
+	dat, err := os.ReadFile(scanner.Text())
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println(string(dat))
 	}
-	file, err := os.Open(fileLocation)
+	file, err := os.Open(scanner.Text())
 	if err != nil {
 		fmt.Println(err)
 	}
-	b1 := make([]byte, 13)
+	b1 := make([]byte, 60)
 	n1, err := file.Read(b1)
 	if err != nil {
 		fmt.Println("Couldnt read bytes.", err)
 	}
-	fmt.Printf("%d bytes : %s\n", n1, string(b1[:n1]))
+	fmt.Println(string(b1[:n1]))
 }
